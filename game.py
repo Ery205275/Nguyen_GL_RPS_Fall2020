52cee721ae1ba9beff76c8755dc704f31a7d8a9f
Nguyen Gia Linh <nguyengialinh@Nguyens-MacBook-Pro.local>
2020-11-28 16:55:02 -0500

    adding lives to game.py

--- game.py
+++ game.py
@@ -4,29 +4,66 @@ from random import randint
 # the index always starts at 0
 choices = ["rock", "paper", "scissors"]
 
-#this is player choice
-player = input("Choose rock, paper or scissors: ")
+player_lives = 5
+computer_lives = 5
 
-#this is AI choice (random pick)
-computer = choices[randint(0, 2)]
+total_lives = 5
 
-#check to see what the user input
-print("user chose: "+ player)
+#True anf False are Boolen data types -> they are the equivalent of on or off, 1 or 0
+player = False
 
-#validate that the random choice worked for the AI
-print("Ai chose: "+ computer)
+while player is False: 
 
-if (computer == player):
-	print("tie")
+	#this is player choice
+	player = input("Choose rock, paper or scissors: ")
 
-elif (computer == "rock"): 
-	if (player == "scissors"):
-		print("you lose!")
-	else:
-		print("you win!")
+	# player = True ->it has value now
 
-elif (computer == "paper"): 
-	if (player == "rock"):
-		print("you lose!")
-	else:
-		print("you win!")
+	#this is AI choice (random pick)
+	computer = choices[randint(0, 2)]
+
+	#check to see what the user input
+	print("user chose: "+ player)
+
+	#validate that the random choice worked for the AI
+	print("computer chose: "+ computer)
+
+	if (computer == player):
+	    print("Tie!")
+	elif (computer == "rock"): 
+		if (player == "scissors"):
+			print("you lose!")
+			player_lives -= 1
+		else:
+			print("you win!")
+			computer_lives -= 1
+
+	elif (computer == "paper"): 
+		if (player == "rock"):
+			print("you lose!")
+			player_lives -= 1
+		else:
+			print("you win!")
+			computer_lives -= 1
+		
+	elif (computer == "scissors"): 
+		if (player == "paper"):
+			print("you lose!")
+			player_lives -= 1
+		else:
+			print("you win!")
+			computer_lives -= 1
+	print("Player has", player_lives, "lives left")
+	print("Computer has", computer_lives, "lives left")
+
+	#make loop keep running by setting player back to False
+	# unset, so that the loop condition will evaluate to True
+	player = False
+
+
+     
+
+     
+
+
+
